00990cfda70ea2636f4393a04a0bef045ed17e0a
rajesh525 <seendripu.rajesh@gmail.com>
2022-06-27 10:30:22 +0530

    terraform

---
+++ s3/s3.tf
@@ -0,0 +1,11 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
---
+++ s3/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+    type = string
+  
+}
+
+variable "bucket_name" {
+    type = string
+  
+}
