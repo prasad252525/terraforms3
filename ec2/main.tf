0b8b997803d09c96ce0ca872c2c0fe2d0e2055ea
prasad252525 <108055714+prasad252525@users.noreply.github.com>
2022-06-30 08:43:03 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
     region = var.region
-    ami    = var.ami
-    instance_type = var.instance_type
   
 }
 module "module" {
   source  = "app.terraform.io/tcsorganisation/module/ec2"
   version = "1.1.2"
-  name =var.name
-  
+  ami     = var.ami
+    instance_type = var.instance_type
+     name =var.name
 }
